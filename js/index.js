6d63c76684a48fd6d5656778e417ab6b4d4a5e8e
unknown <vadivels759@gmail.com>
2026-04-05 16:34:03 +0530

    navbar  added in food store

---
+++ js/index.js
@@ -0,0 +1,54 @@
+// let name="peter";
+// let nam="vel;"
+// console.log(`welcome back ${name} ${nam} `);
+
+
+// let a=2;
+// let b=3;
+// a=5;
+//  b=5;
+// console.log(a+b);
+
+// let s=5;
+// let d=10;
+// a=s+d;
+// b=s-d;
+// a=s-d;
+// console.log(s,d);
+// let a=22;
+// if(a % 2 ===0){
+//     console.log("even");
+
+// }
+// else{
+//     console.log("odd");
+// }
+// let num=0;
+// if(num>0){
+//      console.log("positive");
+    
+
+// }
+// else if(num<0){ 
+//      console.log("nagitive");
+
+
+// }
+// else{
+//      console.log("both");
+
+// } 
+    
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(20,30)
+
+// let a=20;
+// let b=30;
+function sum(){
+    let a=20;
+    let b=30;
+   console.log(a+b);
+ }
+ sum()
