4710f6d9dbed53da306771275018ecdb5a9239eb
unknown <vadivels759@gmail.com>
2026-04-08 21:54:30 +0530

    js practice code add

--- js/index.js
+++ js/index.js
@@ -46,9 +46,73 @@
 
 // let a=20;
 // let b=30;
-function sum(){
-    let a=20;
-    let b=30;
-   console.log(a+b);
- }
- sum()
+// function sum(){
+//     let a=20;
+//     let b=30;
+//    console.log(a+b);
+//  }
+// //  sum()
+// function peter(a,b){
+//   return a+b();
+
+// }
+// const a=peter(a,()=>{
+//   return 2+2
+
+// }
+// )
+// console.log(a)
+// function pass (a,b){
+//   return  a+b
+// }
+// pass(2,3)
+// // console.log(pass())
+// function greet(name, callback) {
+//     console.log("Hello " + name)
+//     callback()
+// }
+
+// function bye() {
+//     console.log("Bye 👋")
+// }
+
+// greet("Peter", bye)
+// var a=setInterval(()=>{
+//   console.log("peter")
+// },
+// )
+// console.dir(first)
+
+// function sum(a,b){
+//   console.log(a+b)
+// }
+
+// var a = setInterval(()=>{
+//   console.log(sum())
+// },1000
+
+// )
+// function sum(a,b){
+//   return a+b();
+// }
+// const a=sum("welcome back ",()=>{
+//   return "peter";
+// });
+//  console.log (a)
+
+// let name=setInterval(()=>{
+//   console.log("hello peter")
+// },2000
+// )
+// const sum=(a,b)=>{
+//   return a+b;
+// }
+// console.log(sum(2,2))
+  
+//  var set= setInterval (() => {
+//   console.log("peter")
+// }, 4000);
+setTimeout(() => {
+  alert=("finish");
+  console.log("peter");
+}, 5000);
